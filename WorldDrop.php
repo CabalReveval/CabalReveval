85ff4263bbbe0e0dbfcfcae589481bc80f7068f0
CabalReveval <78493000+CabalReveval@users.noreply.github.com>
2021-04-25 20:33:49 +0300

    Add files via upload
    pts

---
+++ WorldDrop.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace cyberinferno\Cabal\Helpers\FileSystem\Server;
+
+use cyberinferno\Cabal\Helpers\Exceptions\FileBadFormatException;
+use cyberinferno\Cabal\Helpers\FileSystem\File;
+
+/**
+ * Class WorldDrop
+ *
+ * Helper class to load contents from World_drop.scp file
+ *
+ * @package cyberinferno\Cabal\Helpers\FileSystem\Server
+ */
+class WorldDrop extends File
+{
+    /**
+     * Returns array representation of the World_dop.scp file
+     *
+     * @return array
+     */
+    public function getArray()
+    {
+        $fileContents = $this->getFileContents();
+        return explode("\n", $fileContents);
+    }
+
+    /**
+     * @inheritdoc
+     */
+    protected function validateFormat()
+    {
+        parent::validateFormat();
+        $fileContents = $this->getFileContents();
+        if (substr($fileContents, 0, strlen('[WorldDrop]')) !== '[WorldDrop]') {
+            throw new FileBadFormatException();
+        }
+    }
+}
